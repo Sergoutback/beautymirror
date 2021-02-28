2c91942d0eb24a5e6bb21a06a2bba53267181012
SergoutbackDesktop <webart2013@gmail.com>
2021-03-01 01:09:59 +0300

    010321-0109

---
+++ YouAreBeauty/Assets/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text TextAnswer;
+    public string text;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+        TextAnswer.text =("Тебе чего?").ToString();
+        
+    }
+
+    // Update is called once per frame
+    public void On_Click_button()
+    {
+        TextAnswer.text =("Конечно!").ToString();
+        Handheld.Vibrate();  
+    }
+}
