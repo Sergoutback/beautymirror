d83b6fc47383c6875b76bdb559079e7f3c64659e
SergoutbackDesktop <webart2013@gmail.com>
2021-03-03 16:44:47 +0300

    03032021_1643
    Add Start and Switch buttons

--- YouAreBeauty/Assets/ButtonScript.cs
+++ YouAreBeauty/Assets/ButtonScript.cs
@@ -8,10 +8,11 @@ public class ButtonScript : MonoBehaviour
 {
     public Text TextAnswer;
     public string text;
+    // public object TextAnswer { get; private set; }
+
     // Start is called before the first frame update
     public void Start()
     {
-        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
         TextAnswer.text =("Тебе чего?").ToString();
         
     }
@@ -19,7 +20,7 @@ public class ButtonScript : MonoBehaviour
     // Update is called once per frame
     public void On_Click_button()
     {
-        TextAnswer.text =("Конечно!").ToString();
+        TextAnswer.text =("Конечно да! \nТы самая\nкрасивая!").ToString();
         Handheld.Vibrate();  
     }
 }
