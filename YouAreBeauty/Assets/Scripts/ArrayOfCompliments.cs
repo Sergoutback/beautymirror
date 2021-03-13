09f0f08e9f8575e5fd37664fc0e08e61cef3b50c
SergoutbackDesktop <webart2013@gmail.com>
2021-03-13 19:08:25 +0300

    Revert "Revert "13032021_0108""
    This reverts commit f94d59026ca7fa359bba75b867ffc06e9b19b427.

--- YouAreBeauty/Assets/Scripts/ArrayOfCompliments.cs
+++ YouAreBeauty/Assets/Scripts/ArrayOfCompliments.cs
@@ -14,16 +14,68 @@ public class ArrayOfCompliments : MonoBehaviour
         // TextArrOfCompl.text = ("Script2 start").ToString();
         List<string> myArr = new List<string>
             {
-            "Yes, of course! \nYou are\nso beauty!",
-            "Саша",
-            "Костя",
-            "Маша",
-            "Никита",
-            "Диана",
-            "Даша",
-            "Лёша",
-            "Влад Быков",
-            "Влад Волков",
+            "Yes, of course! You are so beauty!",
+            "Your mind is just as sexy as your beauty",
+            "I miss your smile",
+            "You’re an amazing friend",
+            "I can’t believe I found someone like you",
+            "I get excited every time I see you",
+            "I love making you laugh",
+            "You’re my best friend",
+            "I’ll always have your back",
+            "I love every inch of you— even your toes",
+            "You’ve got a sexy personality",
+            "You’re my queen",
+            "I miss you even when you haven’t left yet",
+            "I feel lucky to be with you",
+            "You always know how to make me feel at home",
+            "I can fully be myself with you",
+            "I love your style",
+            "I love when you take care of me, now it’s your turn", 
+            "You complete everything I’ve ever wanted in a woman",
+            "You look beautiful even when you don’t try",
+            "You make me want to be my best because you deserve the best",
+            "You’re even sexy when you’re bossy",
+            "The more I learn about you, the more I fall",
+            "You’re even more beautiful in the morning",
+            "I love talking to you",
+            "I wouldn’t mind growing old and fat with you",
+            "Your lips always look kissable",
+            "I could kiss you forever",
+            "I could have sex with you forever",
+            "You’re a great kisser",
+            "You’re the best at cuddling", 
+            "You could pick your nose and I still wouldn’t be turned-off",
+            "You deserve the world",
+            "I never knew what it meant to be a man until I met you",
+            "The best part of my day is waking up next to you",
+            "I have fun just being beside you doing nothing",
+            "You have my heart",
+            "You’re the better part of me",
+            "You showed me I’m capable of having the best relationship in the world",
+            "I won the jackpot with you",
+            "You deserve the most beautiful things", 
+            "You’re a gem",
+            "You’re my family",
+            "Even when you’re grouchy, I love you", 
+            "I’d climb a mountain for you",
+            "If we were in the Middle Ages, I’d fight a dragon for you",
+            "I’m always looking forward to our next adventure",
+            "You make me happy",
+            "You have all my respect",
+            "You make me feel like I can touch the sky",
+            "The hardest part of hugging you is letting you go",
+            "You have the sweetest heart",
+            "You taught me the difference between a girl and a woman",
+            "I love looking at you",
+            "You light up my life like nobody else",
+            "Your cooking is amazing",
+            "I never thought it could be this good", 
+            "I feel most alive by your side",
+            "I smile thinking about you when I’m alone",
+            "You make commitment feel easy",
+            "You’re everything to me",
+
             };
             System.Random rand = new System.Random();
 
@@ -35,47 +87,3 @@ public class ArrayOfCompliments : MonoBehaviour
                 }
     }
 }
-
-
-
-
-
-// namespace WindowsFormsApplication1
-// {
-//     public partial class Form1 : Form
-//     {
-//         public Form1()
-//         {
-//             InitializeComponent();
-//             randText();
-//         }
-//         public void randText()
-//         {
-//             List<string> myArr = new List<string>
-//             {
-//              "Вика",
-//             "Саша",
-//             "Костя",
-//             "Маша",
-//             "Никита",
-//             "Диана",
-//             "Даша",
-//             "Лёша",
-//             "Влад Быков",
-//             "Влад Волков",
-//             };
-//             Random rand = new Random();
-//             for (int i = myArr.Count; i > 0; i--)
-//             {
-//                 int flag = rand.Next(myArr.Count);
-//                 textBox1.Text += s + Environment.NewLine;
-//                 myArr.RemoveAt(flag);
-//             }
-//         }
- 
-//         private void button1_Click(object sender, EventArgs e)
-//         {
-//             this.Close();
-//         }
-//     }
-// }
