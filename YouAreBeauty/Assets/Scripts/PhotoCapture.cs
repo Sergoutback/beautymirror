775b3d705469f97e151ae2b141f525250006b0ef
SergoutbackDesktop <webart2013@gmail.com>
2021-07-05 21:15:33 +0300

    05072021_2115
    fix some errors

--- YouAreBeauty/Assets/Scripts/PhotoCapture.cs
+++ YouAreBeauty/Assets/Scripts/PhotoCapture.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PhotoCapture : MonoBehaviour
 {
     public Camera captureCamera;
+    public RawImage displayScreen;
     
     public void MakeScrenshot() 
     {
